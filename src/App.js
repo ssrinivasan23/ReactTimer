0446b577bfcda0d7090b43b418d5d326e4670ca2
ssrinivasan23 <s.srinivasan23@gmail.com>
2021-02-01 08:07:53 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component, useState } from "react";
+import Timer from './timer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Timer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
